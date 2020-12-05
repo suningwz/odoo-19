a2a45588846fd813b126cbb14add83d364ad477e
delegalog <74094428+delegalog@users.noreply.github.com>
2020-12-05 18:16:35 +0100

    res

--- odoo_multi_channel_sale/models/base/multi_channel_sale.py
+++ odoo_multi_channel_sale/models/base/multi_channel_sale.py
@@ -237,7 +237,7 @@ class MultiChannelSale(models.Model):
 		help='Stock Location used for imported product.',
 	)
 	company_id = fields.Many2one(
-		related='warehouse_id.company_id',
+		comodel_name='res.company',
 		string='Company ID',
 	)
 
