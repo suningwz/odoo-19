4ea3c420a2a6df71567d6b2aaec1a8d61eb1e80d
delegalog <74094428+delegalog@users.noreply.github.com>
2020-12-05 10:15:44 +0100

    Revert "company"
    This reverts commit 58e2abb22c4176f187f7e4c90cf6ae47dd87e3ee.

--- odoo_multi_channel_sale/models/base/multi_channel_sale.py
+++ odoo_multi_channel_sale/models/base/multi_channel_sale.py
@@ -237,8 +237,7 @@ class MultiChannelSale(models.Model):
 		help='Stock Location used for imported product.',
 	)
 	company_id = fields.Many2one(
-		#related='warehouse_id.company_id',
-		related='res.company',
+		related='warehouse_id.company_id',
 		string='Company ID',
 	)
 
