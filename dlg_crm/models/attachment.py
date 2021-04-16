0bf7f9a510bcd659eb6faa39f75f3fb99cbb32f7
delegalog <sistemas@delegalog.com>
2021-04-16 07:16:25 +0200

    g

--- dlg_crm/models/attachment.py
+++ dlg_crm/models/attachment.py
@@ -3,7 +3,5 @@ from odoo import models, fields
 
 class Attachment(models.Model):
     _inherit = 'ir.attachment'
-    _name = 'dlg_crm.attachment'
-    _description = 'Adjuntos'
 
     action_id = fields.Integer(string="ID acción", readonly=True)
