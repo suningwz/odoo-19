4ca196f9fd46ab8fbe762c023a49e75c46c18c75
delegalog <sistemas@delegalog.com>
2021-04-17 17:31:56 +0200

    l

--- dlg_crm/models/action.py
+++ dlg_crm/models/action.py
@@ -43,17 +43,6 @@ class Action(models.Model):
             'context': {},
         }
 
-    @staticmethod
-    def f_create_attachment(self):
-        return {
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'dlg_crm.attachment',
-            'view_id': self.env.ref("dlg_crm.view_dlg_crm_attachment_form").id,
-            'type': 'ir.actions.act_window',
-            'context': {},
-        }
-
     def f_search_update(self):
         action = self.env['dlg_crm.action'].search([('name', '=', 'ORM test')])
         print('search()', action, action.name)
