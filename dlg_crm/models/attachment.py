e22e509a53dbcc689a422ecb4b7d7c89b202e925
delegalog <sistemas@delegalog.com>
2021-04-17 09:16:17 +0200

    Update attachment.py

--- dlg_crm/models/attachment.py
+++ dlg_crm/models/attachment.py
@@ -6,5 +6,5 @@ class Attachment(models.Model):
     _inherit = 'ir.attachment'
     _description = 'Adjuntos'
 
-    #action_id = fields.Many2one('dlg_crm.action', 'attachments', string="Adjunto")
-    action_id = fields.Integer(string="ID acción", readonly=True)
+    action_id = fields.Many2many('dlg_crm.action', 'attachments', string="Adjunto")
+    #action_id = fields.Integer(string="ID acción", readonly=True)
