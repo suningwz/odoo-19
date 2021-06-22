e3412ac9cf7a255f1e047d95f4c161fa5e54a919
delegalog <sistemas@delegalog.com>
2021-06-22 10:33:14 +0200

    rem presta

--- prestashop_connector_gt-14/prestashop_connector_gt/prestapyt/__init__.py
+++
@@ -1,4 +0,0 @@
-from .prestapyt import PrestaShopWebService
-from .prestapyt import PrestaShopWebServiceDict
-from .prestapyt import PrestaShopWebServiceError
-from .prestapyt import PrestaShopAuthenticationError
