d0298ddff07a6747fa4612f9c46d17745dd13bce
delegalog <sistemas@delegalog.com>
2021-06-26 20:37:23 +0200

    Revert "Update product_template.py"
    This reverts commit d3915560c13d9a7579055b0c7b5b680d6c3cbdd8.

--- odoo_multi_channel_sale/models/core/product_template.py
+++ odoo_multi_channel_sale/models/core/product_template.py
@@ -4,52 +4,52 @@
 # See LICENSE file for full copyright and licensing details.
 # License URL : <https://store.webkul.com/license.html/>
 ##############################################################################
-from odoo import fields, models
+from odoo import fields,models
 
 
 class ProductTemplate(models.Model):
 	_inherit = 'product.template'
 
 	wk_parent_default_code = fields.Char('Parent Code')
-	wk_length = fields.Float('Length')
-	width = fields.Float('Width')
-	height = fields.Float('Height')
+	wk_length              = fields.Float('Length')
+	width                  = fields.Float('Width')
+	height                 = fields.Float('Height')
 
 	dimensions_uom_id = fields.Many2one(
-		comodel_name='uom.uom',
-		string='Default Unit of Measure',
-		help="Default Unit of Measure used for dimension."
+		comodel_name = 'uom.uom',
+		string       = 'Default Unit of Measure',
+		help         = "Default Unit of Measure used for dimension."
 	)
 
 	channel_mapping_ids = fields.One2many(
-		string='Mappings',
-		comodel_name='channel.template.mappings',
-		inverse_name='template_name',
-		copy=False
+		string       = 'Mappings',
+		comodel_name = 'channel.template.mappings',
+		inverse_name = 'template_name',
+		copy         = False
 	)
 
 	channel_category_ids = fields.One2many(
-		string='Extra Categories',
-		comodel_name='extra.categories',
-		inverse_name='product_id',
+		string       = 'Extra Categories',
+		comodel_name = 'extra.categories',
+		inverse_name = 'product_id',
 	)
 
 	channel_ids = fields.Many2many(
-		comodel_name='multi.channel.sale',
-		relation='product_tmp_channel_rel',
-		column1='product_tmpl_id',
-		column2='channel_id',
-		string='Channel(s)'
+		comodel_name = 'multi.channel.sale',
+		relation     = 'product_tmp_channel_rel',
+		column1      = 'product_tmpl_id',
+		column2      = 'channel_id',
+		string       = 'Channel(s)'
 	)
 
 	wk_product_id_type = fields.Selection(
 		selection=[
-			('wk_upc', 'UPC'),
-			('wk_ean', 'EAN'),
-			('wk_isbn', 'ISBN'),
+			('wk_upc','UPC'),
+			('wk_ean','EAN'),
+			('wk_isbn','ISBN'),
 		],
-		string='Product ID Type',
-		default='wk_upc',
+		string  = 'Product ID Type',
+		default = 'wk_upc',
 	)
 
 	def _create_variant_ids(self):
